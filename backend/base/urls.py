6a7973313ed6e7130afa181f5d6a097e08a8bb0f
kulpallavi <84001248+kulpallavi@users.noreply.github.com>
2021-07-12 11:07:26 +0530

    Update urls.py

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,21 +2,12 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
-
-# urlpatterns = [
-#     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-# ]
 
 urlpatterns =[
     path('subscriptions/',views.SubscriptionsListView.as_view(), name='sub-list'),
     path('subscriptions/<str:pk>',views.SubscriptionView.as_view(), name='sub'),
 
     path('orders/add/', views.BecomePro.as_view(), name='user-sub'),
-    # path('orders/myorder/', views.GetMyOrder.as_view(), name='user-myorder'),
     path('orders/<str:pk>/', views.GetUserSubscription.as_view(), name='get-sub'),
     path('orders/<str:pk>/pay/', views.UpdateSubscriptionToPaid.as_view(), name='paid-sub'),
 
